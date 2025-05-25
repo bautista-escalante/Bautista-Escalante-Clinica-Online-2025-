de3794ca4244ca4859b45127c5c6541d7d313e55
Your Name <you@example.com>
2025-05-24 21:31:46 -0300

    estructura

---
+++ src/app/seccion-usuario/seccion-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion-usuario',
+  imports: [],
+  templateUrl: './seccion-usuario.component.html',
+  styleUrl: './seccion-usuario.component.css'
+})
+export class SeccionUsuarioComponent {
+
+}
