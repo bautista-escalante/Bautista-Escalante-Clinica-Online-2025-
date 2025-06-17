dae700210b89bff16dd88382d2ad7708bf7a1f16
Your Name <you@example.com>
2025-06-16 23:47:01 -0300

    exel pacientes

--- src/app/seccion-paciente/seccion-paciente.component.ts
+++ src/app/seccion-paciente/seccion-paciente.component.ts
@@ -4,7 +4,7 @@ import { AccesoService } from '../servicios/acceso.service';
 import { UsuarioService } from '../servicios/usuario.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import * as XLSX from 'xlsx'
 @Component({
   selector: 'app-seccion-paciente',
   imports: [CommonModule, RouterLink],
@@ -15,14 +15,34 @@ export class SeccionPacienteComponent implements OnInit {
 
   pacientes: any[] = [];
   datosUsuario: any = "";
+  perfil: string = "";
 
   constructor(private especialistaService: EspecialistaService, private acceso: AccesoService, private usuarioService: UsuarioService) { }
 
   async ngOnInit() {
+    this.perfil = await this.acceso.obtenerPerfil();
     let correo = await this.acceso.verificarAcceso();
     this.datosUsuario = await this.usuarioService.getUserByEmail(correo!);
-    this.pacientes = await this.especialistaService.traerPacientesAtendidos(this.datosUsuario.id);
-    console.log(this.pacientes)
+
+    if (this.perfil === "especialista") {
+      this.especialistaService.traerPacientesAtendidos(this.datosUsuario.id).then(pacientes => {
+        this.pacientes = Array.from(new Set(pacientes))
+      })
+    }
+    if (this.perfil === "admin") {
+      this.pacientes = await this.usuarioService.traerPacientes();
+    }
   }
+
+  descargarExcel() {
+    const data = document.getElementById('data');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(data);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Pacientes');
+
+    XLSX.writeFile(wb, 'pacientes.xlsx');
+  }
+
 
 }
